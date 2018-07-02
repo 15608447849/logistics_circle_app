c62811df0bedee8404b662b20b923625119bb7d8
admin@example.com <793065165@qq.com>
2018-07-02 16:29:16 +0800

    login test .

---
+++ src/pages/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>姓名</p>
+    <input v-model="username"/>
+    <p >{{username}}</p>
+  </div>
+
+</template>
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style/>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// 模块名大写
+// 登陆模块
+import LOGIN from '@/pages/login/login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: LOGIN
+    },
+    {
+      path: '/login',
+      component: LOGIN
     }
   ]
 })
