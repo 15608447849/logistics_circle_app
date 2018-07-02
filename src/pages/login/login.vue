8f73cbe2bb059d1a841692e1bded86429b31a219
admin@example.com <793065165@qq.com>
2018-07-02 18:59:28 +0800

    login test .

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>姓名</p>
-    <input v-model="username"/>
-    <p >{{username}}</p>
+    <button @click="icetest"> 测试 </button>
   </div>
 
 </template>
 <script>
+import { queryIce } from '@/iceMethod/iceClient.js'
+// var sysmanage = require('@/iceMethod/sysManage.js').sysmanage
+// import sys  from '@/iceMethod/sysManage.js'
+
 export default {
+
   data () {
-    return {
-      username: '',
-      password: ''
+    return {}
+  },
+  methods: {
+    icetest: function () {
+      console.log('点击')
+      // var service = ['loginCS', sysmanage.SysManageServicePrx, 'SysManageService']
+      queryIce(15151515, '15608447849', '123456', function (e) {
+        console.log(e)
+      })
     }
   }
 }
