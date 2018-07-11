25bcfed97209f020f1b154550f40b2ed31028ce0
soul <464125574@qq.com>
2018-07-11 18:49:49 +0800

    登录注册，圈子订单切页

--- src/pages/information/issueDetails.vue
+++ src/pages/information/issueDetails.vue
@@ -11,28 +11,28 @@
          <span class="logisticsName">黄旺物流</span>
        </div>
        <ul>
-         <li class="marginBottom10">
+         <li class="marginBottom12">
            <span class="issueText">运单号</span><span>1121515524</span>
          </li>
-         <li class="marginBottom10">
+         <li class="marginBottom12">
            <span class="issueText">出发地</span><span>湖南省，长沙市，长沙县，长沙大道10号</span>
          </li>
-         <li class="marginBottom10">
+         <li class="marginBottom12">
            <span class="issueText">目的地</span><span>广东省，广州市，荔湾区，荔湾大道8号</span>
          </li>
-         <li class="marginBottom10">
-           <span class="issueText">货物信息</span><span>1吨，</span><span>100件，</span><span>木托，</span><span>家具</span>
+         <li class="marginBottom12">
+           <span class="issueText">货物信息</span><span class="textMargin-right10">1吨</span><span class="textMargin-right10">100件</span><span class="textMargin-right10">木托</span><span class="textMargin-right10">家具</span>
          </li>
-         <li class="marginBottom10">
-           <span class="issueText">车辆信息</span><span>1台，</span><span>9.6米，</span><span>厢式货车，</span><span>勿压</span>
+         <li class="marginBottom12">
+           <span class="issueText">车辆信息</span><span class="textMargin-right10">1台</span><span class="textMargin-right10">9.6米</span><span class="textMargin-right10">厢式货车</span><span class="textMargin-right10">勿压</span>
          </li>
-         <li class="marginBottom10">
-           <span class="issueText">费用信息</span><span>无代收，</span><span>无保价，</span><span>线下支付3000元整单</span><span></span>
+         <li class="marginBottom12">
+           <span class="issueText">费用信息</span><span class="textMargin-right10">无代收</span><span class="textMargin-right10">无保价</span><span class="textMargin-right10">线下支付3000元整单</span><span></span>
          </li>
-         <li class="marginBottom10">
-           <span class="issueText">时间信息</span><span class="textMargin-right10">2018-6-27</span>  <span>13:00-18:00取货</span>
+         <li class="marginBottom12">
+           <span class="issueText">时间信息</span><span class="textMargin-right10">2018-6-27</span>  <span>13:00-18:00</span><span>取货</span>
          </li>
-         <li class="marginBottom10">
+         <li class="marginBottom12">
            <span class="issueText">联系方式</span><span class="textMargin-right10">18373270790</span><span>18373270790</span>
          </li>
        </ul>
@@ -40,15 +40,15 @@
      <div class="lineHeight10">     </div>
      <div class="orderPadding10">
        <ul>
-         <li class="marginBottom10">
-           <span class="issueText">收货人</span>
-           <span class="textMargin-right10">老王</span>
-           <span class="textMargin-right10">18373270790</span>
-           <span class="textMargin-right10">送货-不上楼</span>
-           <span class="textMargin-right10">原件返回</span>
+         <li class="marginBottom12">
+           <span class="Consignee">收货人</span>
+           <span class="marginright3">老王</span>
+           <span class="marginright3">18373270790</span>
+           <span class="marginright3">送货-不上楼</span>
+           <span class="marginright3">原件返回</span>
          </li>
-         <li class="marginBottom10">
-           <span class="issueText">到货时间</span>
+         <li class="marginBottom12">
+           <span class="arrivalTime">到货时间</span>
            <span>2018-05-03</span>
            <span>至</span>
            <span>2018-06-01</span>
@@ -60,31 +60,31 @@
              <span class="floatleft rderCost">订单费用</span>
              <span class="textRed textBlod floatright rderCost">3000.00元</span>
            </div>
-           <div class="floatleft margin_bottom140">
-             <ul>
-               <li class="marginBottom10">
-                 <span class="issueText">订单号</span>
-                 <span class="issueDetailsText">f12113213265656132</span>
+           <div class="floatleft margin_bottom140 ulWidth100">
+             <ul class="ulWidth100">
+               <li class="marginBottom12">
+                 <span>订单号</span>
+                 <span>f12113213265656132</span>
                </li>
-               <li class="marginBottom10">
-                 <span class="issueText">发单时间</span>
-                 <span class="issueDetailsText">2018-02-02</span>
-                 <span class="issueDetailsText">09:02</span>
+               <li class="marginBottom12">
+                 <span>发单时间</span>
+                 <span >2018-02-02</span>
+                 <span>09:02</span>
                </li>
-               <li class="marginBottom10">
-                 <span class="issueText">抢单时间：</span>
-                 <span class="issueDetailsText">2018-02-02</span>
-                 <span class="issueDetailsText">09:02</span>
+               <li class="marginBottom12">
+                 <span>抢单时间</span>
+                 <span>2018-02-02</span>
+                 <span>09:02</span>
                </li>
-               <li class="marginBottom10">
-                 <span class="issueText">取货时间</span>
-                 <span class="issueDetailsText">2018-02-02</span>
-                 <span class="issueDetailsText">09:02</span>
+               <li class="marginBottom12">
+                 <span>取货时间</span>
+                 <span>2018-02-02</span>
+                 <span>09:02</span>
                </li>
-               <li class="marginBottom10">
-                 <span class="issueText">签收时间</span>
-                 <span class="issueDetailsText">2018-02-02</span>
-                 <span class="issueDetailsText">09:02</span>
+               <li class="marginBottom12">
+                 <span>签收时间</span>
+                 <span>2018-02-02</span>
+                 <span>09:02</span>
                </li>
              </ul>
            </div>
