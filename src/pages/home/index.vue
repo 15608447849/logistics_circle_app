fb710fae8b41ad78ea92036a93e512048a15bc5d
soul <464125574@qq.com>
2018-07-16 09:22:02 +0800

    登录注册，圈子订单切页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,40 +1,64 @@
 <template>
   <div>
-    <yd-navbar title="首页"></yd-navbar>
+    <!--页头-->
+    <div class="issueHeaderNav">
+      <img src="../../assets/images/small/logo_26.png" alt="" class="issueHeaderNavPic">
+      <span>首页</span>
+      <span @click="toissue" class="icon iconfont icon-gengduo1"></span>
+    </div>
     <yd-button type="primary" style="margin-top: 12%;margin-bottom:2%;" @click.native="toLogin">登陆页面</yd-button>
     <yd-button type="primary" @click.native="toRegister">注册页面</yd-button>
     <yd-button type="primary" @click.native="toPageGeo">地图测试</yd-button>
     <yd-button type="primary" @click.native="toPagePickerTest">picker测试</yd-button>
     <yd-button type="primary" @click.native="toTest">测试页面</yd-button>
-    <yd-button type="primary" @click.native="toSubmitOrder">发单测试</yd-button>
-    <!--首页轮播-->
-    <div class="binnar">
-       <div class="binnar_img">img</div>
-       <!--<img src="" alt="">-->
-    </div>
-    <div class="content">
-        <div class="order">
-           <div class="bill">
-             <div class="order_img"></div>
-             <span>快速发单</span>
-           </div>
-           <div class="snag_order">
-             <div class="order_img"></div>
-             <span>已抢订单</span>
-           </div>
+    <div class="searchBox">
+      <div class="selectRegion">
+        <div class="searchBtn">
+          <i class="icon iconfont icon-sousuo"></i>
+          <span class="sousuo">搜索</span>
         </div>
-        <div class="bold_line"></div>
-        <div class="order_box"><span class="good_order">----为你精选的好订单----</span></div>
-      <!--订单列表-->
-      <ul class="order_box">
-        <li class="order_list">
-          <div class="order_price"><span class="site">长沙</span><span class="site">—</span><span class="site">广州</span><span class="time">1小时前</span></div>
-          <div class="order_time"><span class="volume">家具,9方</span><span class="car_type">厢式货车，9.6</span><span class="price">2000元</span></div>
-        </li>
-      </ul>
-      <a class="more_orders">更多订单</a>
+      </div>
     </div>
+    <div class="orderOperation">
+      <div class="banner">
+        <img src="../../assets/images/small/banner@2x.png" alt="">
+      </div>
+      <div class="fastSendRob">
+        <div class="sendOrder">
+          <img src="../../assets/images/small/快速发单@2x.png" alt="">
+          <span>快速发单</span>
+        </div>
+        <div class="already">
+          <img src="../../assets/images/small/已抢订单@2x.png" alt="">
+          <span>快速发单</span>
+        </div>
+      </div>
+    </div>
+    <ul class="order_box">
+      <li class="order_list" @click="toissueDetails">
+        <div class="order_time"><span class="site">长沙</span><span class="site">—</span><span class="site">广州</span><span class="time">1小时前</span></div>
+        <div class="order_price"><span class="carWeight">家具</span><span class="carWeight">9方</span><span class="carWeight">厢式货车</span><span class="carWeight">9.6</span><span class="total_price">￥2000元</span></div>
+      </li>
+      <li class="order_list" @click="toissueDetails">
+        <div class="order_time"><span class="site">长沙</span><span class="site">—</span><span class="site">广州</span><span class="time">1小时前</span></div>
+        <div class="order_price"><span class="carWeight">家具</span><span class="carWeight">9方</span><span class="carWeight">厢式货车</span><span class="carWeight">9.6</span><span class="total_price">￥2000元</span></div>
+      </li>
+      <li class="order_list" @click="toissueDetails">
+        <div class="order_time"><span class="site">长沙</span><span class="site">—</span><span class="site">广州</span><span class="time">1小时前</span></div>
+        <div class="order_price"><span class="carWeight">家具</span><span class="carWeight">9方</span><span class="carWeight">厢式货车</span><span class="carWeight">9.6</span><span class="total_price">￥2000元</span></div>
+      </li>
+      <li class="order_list" @click="toissueDetails">
+        <div class="order_time"><span class="site">长沙</span><span class="site">—</span><span class="site">广州</span><span class="time">1小时前</span></div>
+        <div class="order_price"><span class="carWeight">家具</span><span class="carWeight">9方</span><span class="carWeight">厢式货车</span><span class="carWeight">9.6</span><span class="total_price">￥2000元</span></div>
+      </li>
+      <li class="order_list" @click="toissueDetails">
+        <div class="order_time"><span class="site">长沙</span><span class="site">—</span><span class="site">广州</span><span class="time">1小时前</span></div>
+        <div class="order_price"><span class="carWeight">家具</span><span class="carWeight">9方</span><span class="carWeight">厢式货车</span><span class="carWeight">9.6</span><span class="total_price">￥2000元</span></div>
+      </li>
+    </ul>
+    <div class="seeMore">查看更多</div>
   </div>
+
 </template>
 <script>
   export default{
@@ -53,9 +77,6 @@
       },
       toTest(){
         this.$router.push({path: '/test'})
-      },
-      toSubmitOrder(){
-        this.$router.push({path: '/infoTest'})
       }
     }
   }
